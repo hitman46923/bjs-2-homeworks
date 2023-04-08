1c80b11955be8e53c09bfab8cb15804aac2a9d7f
hitman46923 <al46923@outlook.com>
2023-04-08 22:49:09 +0300

    complete base concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,67 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+
+
+let d =  b ** 2 - 4 * a * c;
+let arr = [];
+  if (d < 0) {
+      return arr ;
+
+  } else if (d == 0) {
+      let root = -b/(2 *a );
+        arr.push(root);
+
+
+
+  } else {
+    let  root1 = (-b + Math.sqrt(d)) / (2 * a);
+    let root2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push (root1, root2);
+
+  }
+
+
+
+
   return arr;
+ 
+  
+  
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+
+  if (typeof percent !== 'number' || isNaN(percent) || percent < 0) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  }
+
+  if (typeof contribution !== 'number' || isNaN(contribution) || contribution < 0) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+
+  if (typeof amount !== 'number' || isNaN(amount) || amount < 0) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+
+  if (typeof countMonths !== 'number' || isNaN(countMonths) || countMonths < 0 || countMonths % 1 !== 0) {
+    return `Параметр "Количество месяцев" содержит неправильное значение "${countMonths}"`;
+  }
+
+  const monthlyInterest = percent / 100 / 12;
+  const loanBody = amount - contribution;
+  const annuityRate = monthlyInterest + monthlyInterest / ((1 + monthlyInterest) ** countMonths - 1);
+  const monthlyPayment = loanBody * annuityRate;
+  const totalPayment = (monthlyPayment * countMonths).toFixed(2);
+
+  return Number(totalPayment);
+
+
+
+
+
+
+
+
   
 }
