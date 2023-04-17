76e7c2a47a0db045e884d64702b1e5e9f200b77c
hitman46923 <al46923@outlook.com>
2023-04-17 22:10:07 +0300

    add arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	} else {
+		return arr1.every((value, index) => value === arr2[index]);
+	}
+
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	let filtered = users.filter(user => user.gender === gender);
+	if (filtered.length === 0) {
+		return 0;
+	}
+	let totalAge = filtered.map(user => user.age).reduce((sum, age) => sum + age);
+	return ((totalAge / filtered.length));
+
 }
